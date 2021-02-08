7058d39890712474b9b10bc60181878588b2b21c
Jorge Ortiz Fuentes <joortizf@inf.uc3m.es>
2021-02-08 23:15:09 +0100

    Rename activity, fragment and layouts

--- app/src/main/java/com/canonicalexamples/tearank/TeaRankActivity.kt
+++ app/src/main/java/com/canonicalexamples/tearank/TeaRankActivity.kt
@@ -7,11 +7,11 @@ import androidx.appcompat.app.AppCompatActivity
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.google.android.material.snackbar.Snackbar
 
-class MainActivity : AppCompatActivity() {
+class TeaRankActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_tea_rank)
         setSupportActionBar(findViewById(R.id.toolbar))
 
         findViewById<FloatingActionButton>(R.id.fab).setOnClickListener { view ->
